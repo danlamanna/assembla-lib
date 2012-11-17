1848839cde2c26a20a8fe2790b3e4a2279b1e42c
Dan LaManna <dan.lamanna@gmail.com>
2012-11-17 11:50:50 -0500

    - Added custom var assembla-cache-enabled, which will effectively disable any caching and returning of cached responses if set to nil. Defaults to t.
    - Added custom var assembla-cache-duration-default, which will be a duration in seconds to fallback to if cache is enabled and a duration isn't specified.
    
    - Altered assembla-cache-response to take an optional arg duration, skip over the body of the function if cache isn't wanted, and alter the cache filename stored based on duration.
    
    - Altered assembla-get to have optional args use-cache, and cache-duration. Updated docs to explain the uses of both.

--- assembla-lib.el
+++ assembla-lib.el
@@ -36,12 +36,20 @@
   :group 'assembla-lib
   :type  'string)
 
+(defcustom assembla-cache-enabled t
+  "Setting to nil will disable caching of all responses."
+  :group 'assembla-lib
+  :type  'boolean)
+
 (defcustom assembla-cache-dir "~/.emacs.d/tmp/assembla/cache"
   "Path to store cached Assembla API responses."
   :group 'assembla-lib
   :type  'string)
 
-(defvar assembla-buffer-name "-- assembla --")
+(defcustom assembla-cache-duration-default 3600 ; 1 hour
+  "Duration in seconds a response will be cached by default."
+  :group 'assembla-lib
+  :type  'integer)
 
 ;; utils
 (defun assembla-format-api-url(uri type)
@@ -57,27 +65,52 @@
 ;; cache utils
 (defun assembla-invalidate-caches())
 
-(defun assembla-cache-response(url response)
-  "Caches `response' in a file named after an MD5 hash of `url' and a unix timestamp. Stores file in `assembla-cache-dir' which is created if it doesn't exist."
-  (let* ((url-hash       (md5 url))
-	 (unix-timestamp (format-time-string "%s"))
-	 (cache-file     (format "%s/%s.%s.cache" assembla-cache-dir url-hash unix-timestamp)))
-    (if (not (file-exists-p assembla-cache-dir))
-	(make-directory assembla-cache-dir t))
-    (with-temp-file cache-file
-      (insert (format "%s" response)))))
+(defun assembla-cache-response(url response &optional duration)
+  "Caches `response' in a file named after an MD5 hash of `url', a unix timestamp, and the duration to store it.
+   Stores file in `assembla-cache-dir' which is created if it doesn't exist.
+
+   Nothing will be cached if `assembla-cache-enabled' is `nil', or `duration' is < 1."
+  (unless (or (not assembla-cache-enabled)
+	      (not duration)
+	      (<   duration 1))
+    (let* ((url-hash       (md5 url))
+	   (unix-timestamp (format-time-string "%s"))
+	   (cache-duration (or duration assembla-cache-duration-default))
+	   (cache-file     (format "%s/%s.%s.%d.cache" assembla-cache-dir url-hash unix-timestamp cache-duration)))
+      (if (not (file-directory-p assembla-cache-dir))
+	  (make-directory assembla-cache-dir t))
+      (with-temp-file cache-file
+	(insert (format "%s" response))))))
+
+(defun assembla-has-cache(url)  )
 
 ;; request utils
-(defun assembla-get(uri type callback)
+(defun assembla-get(uri type callback &optional use-cache &optional cache-duration)
   "Retrieves Assembla URI asynchronously and calls `callback' when finished.
    `uri' and `type' get passed to `assembla-format-api-url' to form the retrieve
-   URL."
-  (if (not (assembla-credentials-set))
-      (message "Use M-x customize to set assembla-lib settings.")
-    (let ((url (assembla-format-api-url uri type)))
-      (furl-with-header "X-Api-Key" assembla-api-key
-	(furl-with-header "X-Api-Secret" assembla-api-key-secret
-	  (furl-retrieve url callback))))))
+   URL.
+
+   If `use-cache' is t, `assembla-cache-enabled' is t, and `assembla-has-cache' returns
+   a response, no HTTP request will be sent, and `callback' will be applied to the cached response.
+
+   `use-cache' and `cache-duration' are never both utilized, if it returns a cached response via `use-cache',
+    it won't cache. On the other hand if it can't or doesn't `use-cache', and `assembla-cache-enabled' is t,
+    and `cache-duration' or `assembla-cache-duration-default' are > 1, it will cache the response."
+  (lexical-let* ((url            (assembla-format-api-url uri type))
+		 (callback       callback)
+		 (cached         (and assembla-cache-enabled use-cache (assembla-has-cache url)))
+		 (do-cache       (and assembla-cache-enabled (or cache-duration assembla-cache-duration-default)))
+		 (cache-duration (or cache-duration assembla-cache-duration-default)))
+    (if cached
+	(funcall callback (assembla-get-cache (assembla-format-api-url uri type)))
+      (if (not (assembla-credentials-set))
+	  (message "Use M-x customize to set assembla-lib settings.")
+	(furl-with-header "X-Api-Key" assembla-api-key
+	  (furl-with-header "X-Api-Secret" assembla-api-key-secret
+	    (furl-retrieve url (lambda(response)
+				 (if do-cache
+				     (assembla-cache-response url response cache-duration))
+				 (funcall callback response)))))))))
 
 (defun assembla-post-or-put(uri type data-str request-method callback)
   "POST/PUT (determined by `request-method') the data in `data-str' to Assembla URI asynchronously and calls `callback' when finished.
